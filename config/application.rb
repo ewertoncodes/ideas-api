dfe1b355a554434bfccb3e613c7bc5d3666ddc01
ewertonorg <ewertonorg@gmail.com>
2019-07-20 19:46:41 -0300

    remove /

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module IdeaboardApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://myideaboard.herokuapp.com/'
+        origins 'https://myideaboard.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
