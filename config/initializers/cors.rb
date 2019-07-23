56ef5c6d24cd3b8daf5958a46f9785a394e7d92c
ewertonorg <ewertonorg@gmail.com>
2019-07-23 11:16:54 -0300

    add origins lolcahost:8080

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     
-    origins 'https://my-ideas.netlify.com'
+    origins 'http://localhost:8080'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
